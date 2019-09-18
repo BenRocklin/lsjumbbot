0490ac1603cf189c3cdfc766e86f17c3c6374a1c
Ben Rocklin <brocklin@cs.stanford.edu>
2019-09-17 18:08:27 -0700

    Test changes fix

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
-var botID = process.env.BOT_ID;
+var botID = process.env.TEST_BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
-
-  console.log('BEN MESSAGE ' + request.JSON)
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
