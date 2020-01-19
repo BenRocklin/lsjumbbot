2864659a766b0db8128797a0256e3d2e1af92e0e
Ben Rocklin <brocklin@cs.stanford.edu>
2020-01-18 23:52:08 -0800

    Test image sending

--- bot.js
+++ bot.js
@@ -175,16 +175,18 @@ function handleSong(body, options, songCommand, nativeSection) {
         body.attachments = [{
           "type" : "image",
           "url" : sectionUrls[j]
+        }, {
+          "type" : "image",
+          "url" : sectionUrls[j]
         }]
-        postMessageNoEnd(body, options);
+        postMessage(body, options);
       }
     } else {
       console.log(section + " failure")
       body.text = "Sorry, we could not find the " + section + " chart for " + friendlyName;
-      postMessageNoEnd(body, options);
+      postMessage(body, options);
     }
   }
-  botReq.end(JSON.stringify(body));
 }
 
 function handleCool(body, options) {
@@ -211,26 +213,6 @@ function postMessage(body, options) {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
-}
-
-function postMessageNoEnd(body, options) {
-  var botResponse = body.text;
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
 }
 
 
